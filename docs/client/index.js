4901d928a2fc4b21d791e503d3093c34c93f0ae7
J0shisIsland <pwtz19@durham.ac.uk>
2023-03-03 15:17:05 +0000

    added renderRepertoire function to render as list

--- docs/client/index.js
+++ docs/client/index.js
@@ -17,12 +17,23 @@ document.addEventListener('DOMContentLoaded', () =>{
         event.preventDefault();
         fetch('http://127.0.0.1:8090/josh')
             .then(response => response.json())
-            .then(body => 
-               {
-                 //console.log(body); 
-                 document.getElementById('Repertoire').innerHTML=body.repertoire
-                })
+            .then(body => renderRepertoire(body.repertoire))
+               //{
+               //  //console.log(body); 
+               //  document.getElementById('Repertoire').innerHTML=body.repertoire
+               // })
             .catch( (error) => alert(error))
     });
 }
 )
+
+function renderRepertoire (rep){
+    //alert(rep);
+    let container = document.getElementById('Repertoire');
+    container.innerHTML = "";
+    for(let piece of rep){
+       let item = document.createElement('li')
+       item.innerHTML = piece;
+       container.appendChild(item);
+    }
+    }
