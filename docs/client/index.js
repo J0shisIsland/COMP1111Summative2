9b41ac6b98a8f20e982368adef0ef729f22f575f
J0shisIsland <pwtz19@durham.ac.uk>
2023-03-10 16:01:40 +0000

    fixed errors with eslint

--- docs/client/index.js
+++ docs/client/index.js
@@ -1,53 +1,50 @@
-document.addEventListener('DOMContentLoaded', () =>{
-    document.getElementById("Level")
-    .addEventListener('click', function (event){
-        event.preventDefault();
-        fetch('http://127.0.0.1:8090/josh')
-         .then(response => response.json())
-         .then(body => 
-            {
-             //console.log(body); 
-             document.getElementById('Level').innerHTML=body.level
-            })
-         .catch( (error) => alert(error))
+function renderRepertoire(rep) {
+  // alert(rep);
+  const container = document.getElementById('Repertoire');
+  container.innerHTML = '';
+  for (const piece of rep) {
+    const item = document.createElement('li');
+    item.innerHTML = piece;
+    container.appendChild(item);
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('Level')
+    .addEventListener('click', (event) => {
+      event.preventDefault();
+      fetch('http://127.0.0.1:8090/josh')
+        .then((response) => response.json())
+        .then((body) => {
+          // console.log(body);
+          document.getElementById('Level').innerHTML = body.level;
+        })
+        .catch((error) => alert(error));
     });
 
-    let getRepertoire = document.getElementById("Repertoire");
-
-     fetch('http://127.0.0.1:8090/josh')
-        .then(response => response.json())
-        .then(body => renderRepertoire(body.repertoire))
-            //{
-            //  //console.log(body); 
-            //  document.getElementById('Repertoire').innerHTML=body.repertoire
-            // })
-        .catch( (error) => alert(error))
-    });
-
-function renderRepertoire (rep){
-    //alert(rep);
-    let container = document.getElementById('Repertoire');
-    container.innerHTML = "";
-    for(let piece of rep){
-       let item = document.createElement('li')
-       item.innerHTML = piece;
-       container.appendChild(item);
-    }
-};
-
-let submit = document.getElementById('submit_piece');
-
-submit.addEventListener('click', async function(event){
-   event.preventDefault();
-   let newpiece = document.getElementById('newpiece').value;
-   let parameters = {'newpiece': newpiece};
-   let response = await fetch('http://127.0.0.1:8090/josh/add',{
-   method: 'POST',
-   headers: {
-      'Content-Type': 'application/json'
-   },
-   body: JSON.stringify(parameters)
+  fetch('http://127.0.0.1:8090/josh')
+    .then((response) => response.json())
+    .then((body) => renderRepertoire(body.repertoire))
+  // {
+  //  //console.log(body);
+  //  document.getElementById('Repertoire').innerHTML=body.repertoire
+  // })
+    .catch((error) => alert(error));
 });
-let body = await response.json();
-renderRepertoire(body);
-})
+
+const submit = document.getElementById('submit_piece');
+
+submit.addEventListener('click', async (event) => {
+  event.preventDefault();
+  const newpiece = document.getElementById('newpiece').value;
+  const parameters = { newpiece };
+  const response = await fetch('http://127.0.0.1:8090/josh/add', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(parameters),
+  });
+  const body = await response.json();
+  renderRepertoire(body);
+});
