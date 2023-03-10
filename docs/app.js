8fdb9dbc8e3db8c5fb41d0045e1e53fb308796f6
J0shisIsland <pwtz19@durham.ac.uk>
2023-03-10 15:38:56 +0000

    configured eslint and added pretest script

--- docs/app.js
+++ docs/app.js
@@ -1,27 +1,38 @@
 const express = require('express');
+
 const app = express();
 app.use(express.static('client'));
 app.use(express.json()); // Parse URL-encoded bodies
 
-const Joshua = {
-    level: ["Grade 8"],
-    repertoire: ["Bach Art of the Fugue", 
-        "Beethoven String Quartet", 
-        "Schubert Trio", 
-        "Bach Double Concert"]
+class musical {
+  constructor(id, title, composer, scoring, star) {
+    this.id = id;
+    this.title = title;
+    this.composer = composer;
+    this.scoring = scoring;
+    this.star = star;
+  }
 }
 
-app.get('/josh', function(req, resp){
-    resp.send(Joshua)
-})
+const Joshua = {
+  level: ['Grade 8'],
+  repertoire: ['Bach Art of the Fugue',
+    'Beethoven String Quartet',
+    'Schubert Trio',
+    'Bach Double Concert'],
+};
 
-app.post('/josh/add', function (req, resp) {
-    // console.log(req)
-   console.log(req.body);
-    // resp.send("I am Posting!");
-    const newpiece = req.body.newpiece;
-    Joshua.repertoire.push(newpiece);
-    resp.json(Joshua.repertoire);
-  });
+app.get('/josh', (req, resp) => {
+  resp.send(Joshua);
+});
+
+app.post('/josh/add', (req, resp) => {
+  // console.log(req)
+  console.log(req.body);
+  // resp.send("I am Posting!");
+  const { newpiece } = req.body;
+  Joshua.repertoire.push(newpiece);
+  resp.json(Joshua.repertoire);
+});
 
 module.exports = app;
