f6cc5c56d0e93c22a0e88216c99bf427a7fa55b6
J0shisIsland <pwtz19@durham.ac.uk>
2023-03-23 11:51:23 +0000

    fixed eslint errors and added jest test script

---
+++ docs/app.test.js
@@ -0,0 +1,71 @@
+/* eslint-disable no-undef */
+
+const request = require('supertest');
+const app = require('./app');
+
+describe('Test the Musical Database', () => {
+  // test POSTing a new musical
+  test('POST /musicals/add succeeds', () => {
+    const parameters = {
+      title: 'Firebringer',
+      composer: 'Starkidz',
+      voices: 'Soprano, Alto, Tenor, Bass',
+    };
+    return request(app)
+      .post('/musicals/add')
+      .send(parameters)
+      .expect(200);
+  });
+
+  // test GET /musicals
+  test('GET /musicals succeeds', () => request(app)
+    .get('/musicals')
+    .expect(200));
+
+  test('GET /musicals returns JSON', () => request(app)
+    .get('/musicals')
+    .expect('Content-type', /json/));
+
+  test('GET /musicals includes Starkidz', () => request(app)
+    .get('/musicals')
+    .expect(/Starkidz/));
+
+  // test title search
+  test('GET /musicals/search?field=title&query=Fire succeeds', () => request(app)
+    .get('/musicals/search?field=title&query=Fire')
+    .expect(200));
+
+  test('GET /musicals/search?field=title&query=Fire returns JSON', () => request(app)
+    .get('/musicals/search?field=title&query=Fire')
+    .expect('Content-type', /json/));
+
+  test('GET /musicals/search?field=title&query=Fire includes Fire', () => request(app)
+    .get('/musicals/search?field=title&query=Fire')
+    .expect(/Fire/));
+
+  // test composer search
+  test('GET /musicals/search?field=composer&query=Starkidz succeeds', () => request(app)
+    .get('/musicals/search?field=composer&query=Starkidz')
+    .expect(200));
+
+  test('GET /musicals/search?field=composer&query=Starkidz returns JSON', () => request(app)
+    .get('/musicals/search?field=composer&query=Starkidz')
+    .expect('Content-type', /json/));
+
+  test('GET /musicals/search?field=composer&query=Starkidz includes Starkidz', () => request(app)
+    .get('/musicals/search?field=composer&query=Starkidz')
+    .expect(/Starkidz/));
+
+  // test vocal part search
+  test('GET /musicals/search?field=voices&query=Bass succeeds', () => request(app)
+    .get('/musicals/search?field=voices&query=Bass')
+    .expect(200));
+
+  test('GET /musicals/search?field=voices&query=Bass returns JSON', () => request(app)
+    .get('/musicals/search?field=voices&query=Bass')
+    .expect('Content-type', /json/));
+
+  test('GET /musicals/search?field=voices&query=Bass includes Bass', () => request(app)
+    .get('/musicals/search?field=voices&query=Bass')
+    .expect(/Bass/));
+});
